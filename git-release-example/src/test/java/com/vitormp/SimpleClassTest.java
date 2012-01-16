c7798da90cc0f53bbe69540377c6e350bcd05838
Vitor Monteiro Puente <vitor.puente@maplink.com.br>
2012-01-16 12:26:30 -0200

    Just some file to test the maven release plugin.

---
+++ git-release-example/src/test/java/com/vitormp/SimpleClassTest.java
@@ -0,0 +1,13 @@
+package com.vitormp;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class SimpleClassTest extends TestCase {
+
+	@Test
+	public void testname() throws Exception {
+		
+	}
+}
